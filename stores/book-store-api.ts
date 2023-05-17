e447c1b67abc3e80ca7b0cae632315ec59f75904
William Chong <me@williamchong.cloud>
2023-05-17 17:34:41 +0800

    🎨 Rename wallet variables

--- stores/book-store-api.ts
+++ stores/book-store-api.ts
@@ -6,9 +6,9 @@ export const useBookStoreApiStore = defineStore('book-api', () => {
   const walletStore = useWalletStore()
   const { wallet: storeWallet } = storeToRefs(walletStore)
   const token = ref('')
-  const wallet = ref('')
+  const sessionWallet = ref('')
 
-  const isAuthenticated = computed(() => storeWallet.value === wallet.value && !!token.value)
+  const isAuthenticated = computed(() => storeWallet.value === sessionWallet.value && !!token.value)
 
   async function authenticate (inputWallet: string, signature: any) {
     const { error, data } = await useFetch(`${LIKE_CO_API}/wallet/authorize`, {
@@ -18,7 +18,7 @@ export const useBookStoreApiStore = defineStore('book-api', () => {
     if (error.value) { throw error.value }
     if ((!data?.value as any)?.token) { throw new Error('INVALID_SIGNATURE') }
     token.value = (data.value as any).token
-    wallet.value = inputWallet
+    sessionWallet.value = inputWallet
   }
 
   async function newBookListing (classId: string, payload: any) {
@@ -37,7 +37,7 @@ export const useBookStoreApiStore = defineStore('book-api', () => {
 
   return {
     token,
-    wallet,
+    wallet: sessionWallet,
     isAuthenticated,
     authenticate,
     newBookListing
