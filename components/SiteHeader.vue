7c2c964c5069b99c1f9bc2de59f2fdc255b6a2e6
Ng Wing Tat, David <i@ngwingt.at>
2023-11-13 21:25:11 +0800

    🧑‍💻 Show Testnet badge

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,12 +1,20 @@
 <template>
   <header class="shadow-md">
     <UContainer class="flex justify-between items-center gap-4 py-4">
-      <h1>
+      <h1 class="flex items-center">
         <UButton
           class="font-bold"
           variant="ghost"
           label="LikeCoin NFT Book Press"
           :to="{ name: 'index' }"
+        />
+        <UBadge
+          v-if="IS_TESTNET"
+          label="TESTNET"
+          variant="subtle"
+          color="amber"
+          size="xs"
+          :ui="{ rounded: 'rounded-full', font: 'font-mono' }"
         />
       </h1>
 
@@ -17,3 +25,7 @@
     </UContainer>
   </header>
 </template>
+
+<script setup lang="ts">
+import { IS_TESTNET } from '~/constant'
+</script>
