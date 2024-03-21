060b0e972518f9cb9855c71aa0d3dda5f031cfa1
William Chong <me@williamchong.cloud>
2024-03-21 20:13:23 +0800

    ✨ Add commission history in book store user page

--- pages/nft-book-store/user/index.vue
+++ pages/nft-book-store/user/index.vue
@@ -19,116 +19,165 @@
       description="A Stripe Express account is separated from any full-featured Stripe account. If you have a full-featured Stripe account, you must also register again here to create a Stripe Express account under Liker Land."
     />
 
-    <UCard
-      v-if="bookStoreApiStore.isAuthenticated"
-      :ui="{
-        header: { base: 'flex justify-between items-center' },
-        body: { padding: '' },
-        footer: { base: 'text-center' },
-      }"
-    >
-      <template #header>
-        <h1 class="text-center font-bold font-mono">
-          Book Affiliation Program
-        </h1>
-      </template>
-      <div v-if="likerIdInfo?.user">
-        <h2>
-          Your affiliation channel ID
-        </h2>
-        <code class="text-center font-bold font-mono">
-          @{{ likerIdInfo?.user }}
-        </code>
-        <p v-if="!connectStatus.isReady">
-          Please setup your stripe account below to participate in the book affiliation program.
-        </p>
-        <p v-else>
-          Append <code class="font-bold font-mono">?from=@{{ likerIdInfo?.user }}</code> in any book store page to earn commission from book sales.
-        </p>
-      </div>
-      <p v-else>
-        You have not setup your channel ID yet.
-      </p>
-      <template #footer>
-        <UButton
-          v-if="!likerIdInfo?.user"
-          label="Setup your ID"
-          size="lg"
-          rel="noopener"
-          target="_blank"
-          :to="`${LIKE_CO_HOST}/in`"
-        />
-      </template>
-    </UCard>
-    <UCard
-      v-if="bookStoreApiStore.isAuthenticated"
-      :ui="{
-        header: { base: 'flex justify-between items-center' },
-        body: { padding: '' },
-        footer: { base: 'text-center' },
-      }"
-    >
-      <template #header>
-        <h1 class="text-center font-bold font-mono">
-          Stripe Connect Status
-        </h1>
-
-        <UTooltip
-          text="Refresh Status"
-          :popper="{ placement: 'left' }"
-        >
-          <UButton
-            icon="i-heroicons-arrow-path"
-            variant="outline"
-            :disabled="isLoading"
-            @click="loadStripeConnectStatus"
-          />
-        </UTooltip>
-      </template>
-
-      <UTable
-        :columns="[
-          { key: 'initiated', label: 'Setup Initiated' },
-          { key: 'completed', label: 'Setup Completed' }
-        ]"
-        :rows="[{
-          initiated: connectStatus?.hasAccount || false,
-          completed: connectStatus?.isReady || false
-        }]"
-        :ui="{ th: { base: 'text-center' }, td: { base: 'text-center' } }"
+    <template v-if="bookStoreApiStore.isAuthenticated">
+      <UCard
+        :ui="{
+          header: { base: 'flex justify-between items-center' },
+          body: { padding: '' },
+          footer: { base: 'text-center' },
+        }"
       >
-        <template #initiated-data="{ row }">
-          <UBadge v-if="row.initiated" label="Yes" color="green" variant="outline" />
-          <UBadge v-else label="No" color="red" variant="outline" />
+        <template #header>
+          <h1 class="text-center font-bold font-mono">
+            Book Affiliation Program
+          </h1>
         </template>
-        <template #completed-data="{ row }">
-          <UBadge v-if="row.completed" :label="connectStatus?.email || 'Yes'" color="green" variant="outline" />
-          <UBadge v-else label="No" color="red" variant="outline" />
+        <div v-if="likerIdInfo?.user">
+          <h2>
+            Your affiliation channel ID
+          </h2>
+          <code class="text-center font-bold font-mono">
+            @{{ likerIdInfo?.user }}
+          </code>
+          <p v-if="!connectStatus.isReady">
+            Please setup your stripe account below to participate in the book affiliation program.
+          </p>
+          <p v-else>
+            Append <code class="font-bold font-mono">?from=@{{ likerIdInfo?.user }}</code> in any book store page to earn commission from book sales.
+          </p>
+        </div>
+        <p v-else>
+          You have not setup your channel ID yet.
+        </p>
+        <template #footer>
+          <UButton
+            v-if="!likerIdInfo?.user"
+            label="Setup your ID"
+            size="lg"
+            rel="noopener"
+            target="_blank"
+            :to="`${LIKE_CO_HOST}/in`"
+          />
         </template>
-      </UTable>
+      </UCard>
+      <UCard
+        :ui="{
+          header: { base: 'flex justify-between items-center' },
+          body: { padding: '' },
+          footer: { base: 'text-center' },
+        }"
+      >
+        <template #header>
+          <h1 class="text-center font-bold font-mono">
+            Stripe Connect Status
+          </h1>
 
-      <template #footer>
-        <UButton
-          v-if="isLoading"
-          label="Loading"
-          size="lg"
-          :loading="true"
-          @click="onLoginToStripe"
-        />
-        <UButton
-          v-else-if="connectStatus?.isReady"
-          label="Login to Stripe account"
-          size="lg"
-          @click="onLoginToStripe"
-        />
-        <UButton
-          v-else
-          label="Setup Stripe Payment Recipient Account"
-          size="lg"
-          @click="onSetupStripe"
-        />
-      </template>
-    </UCard>
+          <UTooltip
+            text="Refresh Status"
+            :popper="{ placement: 'left' }"
+          >
+            <UButton
+              icon="i-heroicons-arrow-path"
+              variant="outline"
+              :disabled="isLoading"
+              @click="loadStripeConnectStatus"
+            />
+          </UTooltip>
+        </template>
+
+        <UTable
+          :columns="[
+            { key: 'initiated', label: 'Setup Initiated' },
+            { key: 'completed', label: 'Setup Completed' }
+          ]"
+          :rows="[{
+            initiated: connectStatus?.hasAccount || false,
+            completed: connectStatus?.isReady || false
+          }]"
+          :ui="{ th: { base: 'text-center' }, td: { base: 'text-center' } }"
+        >
+          <template #initiated-data="{ row }">
+            <UBadge v-if="row.initiated" label="Yes" color="green" variant="outline" />
+            <UBadge v-else label="No" color="red" variant="outline" />
+          </template>
+          <template #completed-data="{ row }">
+            <UBadge v-if="row.completed" :label="connectStatus?.email || 'Yes'" color="green" variant="outline" />
+            <UBadge v-else label="No" color="red" variant="outline" />
+          </template>
+        </UTable>
+
+        <template #footer>
+          <UButton
+            v-if="isLoading"
+            label="Loading"
+            size="lg"
+            :loading="true"
+            @click="onLoginToStripe"
+          />
+          <UButton
+            v-else-if="connectStatus?.isReady"
+            label="Login to Stripe account"
+            size="lg"
+            @click="onLoginToStripe"
+          />
+          <UButton
+            v-else
+            label="Setup Stripe Payment Recipient Account"
+            size="lg"
+            @click="onSetupStripe"
+          />
+        </template>
+      </UCard>
+      <UCard
+        :ui="{
+          header: { base: 'flex justify-between items-center' },
+          body: { padding: '' },
+          footer: { base: 'text-center' },
+        }"
+      >
+        <template #header>
+          <h1 class="text-center font-bold font-mono">
+            Book Sales Commission History
+          </h1>
+
+          <UTooltip
+            text="Refresh Status"
+            :popper="{ placement: 'left' }"
+          >
+            <UButton
+              icon="i-heroicons-arrow-path"
+              variant="outline"
+              :disabled="isLoading"
+              @click="loadCommissionHistory"
+            />
+          </UTooltip>
+        </template>
+
+        <UTable
+          :columns="[
+            { key: 'timestamp', label: 'Timestamp' },
+            { key: 'type', label: 'Commission Type' },
+            { key: 'amount', label: 'Commission' },
+            { key: 'classId', label: 'Book Id' },
+            { key: 'collectionId', label: 'Book Collection Id' },
+            { key: 'amountTotal', label: 'Sale Amount' },
+          ]"
+          :rows="commissionHistoryRows"
+          :ui="{ th: { base: 'text-center' }, td: { base: 'text-center' } }"
+        >
+          <template #classId-data="{ row }">
+            <a :href="`${LIKER_LAND_URL}/nft/class/${row.classId}`" target="_blank">
+              {{ row.classId }}
+            </a>
+          </template>
+          <template #collectionId-data="{ row }">
+            <a :href="`${LIKER_LAND_URL}/nft/collection/${row.collectionId}`" target="_blank">
+              {{ row.collectionId }}
+            </a>
+          </template>
+        </UTable>
+      </UCard>
+    </template>
   </main>
 </template>
 
@@ -136,7 +185,7 @@
 import { storeToRefs } from 'pinia'
 import { useBookStoreApiStore } from '~/stores/book-store-api'
 import { useWalletStore } from '~/stores/wallet'
-import { LIKE_CO_API, LIKE_CO_HOST } from '~/constant'
+import { LIKER_LAND_URL, LIKE_CO_API, LIKE_CO_HOST } from '~/constant'
 
 const walletStore = useWalletStore()
 const bookStoreApiStore = useBookStoreApiStore()
@@ -147,15 +196,50 @@ const error = ref('')
 const isLoading = ref(false)
 const connectStatus = ref<any>({})
 const likerIdInfo = ref<any>({})
+const commissionHistory = ref<any>([])
 
 watch(isLoading, (newIsLoading) => {
   if (newIsLoading) { error.value = '' }
 })
 
 onMounted(async () => {
-  await loadLikerId()
-  await loadStripeConnectStatus()
+  await Promise.all([
+    loadCommissionHistory(),
+    loadLikerId(),
+    loadStripeConnectStatus()
+  ])
 })
+
+const commissionHistoryRows = computed(() => {
+  return commissionHistory.value.map((row: any) => {
+    return {
+      ...row,
+      amount: `${row.currency}${row.amount / 100}`,
+      amountTotal: `${row.currency}${row.amountTotal / 100}`,
+      timestamp: new Date(row.timestamp).toLocaleString()
+    }
+  })
+})
+
+async function loadCommissionHistory () {
+  try {
+    isLoading.value = true
+    const { data, error: fetchError } = await useFetch(`${LIKE_CO_API}/likernft/book/user/commissions/list`, {
+      headers: {
+        authorization: `Bearer ${token.value}`
+      }
+    })
+    if (fetchError.value && fetchError.value?.statusCode !== 404) {
+      throw new Error(fetchError.value.toString())
+    }
+    commissionHistory.value = (data.value as any)?.commissions || []
+  } catch (e) {
+    console.error(e)
+    error.value = (e as Error).toString()
+  } finally {
+    isLoading.value = false
+  }
+}
 
 async function loadLikerId () {
   try {
