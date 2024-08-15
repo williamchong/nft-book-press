dce5756ed0f8aa64dee324881e9f83fb7a209003
AuroraHuang22 <75730405+AuroraHuang22@users.noreply.github.com>
2024-08-15 16:35:44 +0800

    🐛 Fix missing signer issue during redirect for AuthCore (#175)
    * 🎨 Merge sign in methods to header
    
    * 🐛 Fix missing signer issue during redirect for AuthCore
    
    * 🥅 Handle missing signer issue

--- pages/auth/redirect.vue
+++ pages/auth/redirect.vue
@@ -8,13 +8,22 @@
   </div>
 </template>
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
 import { useWalletStore } from '~/stores/wallet'
+import { useBookStoreApiStore } from '~/stores/book-store-api'
 
 const store = useWalletStore()
 const { handleConnectorRedirect } = store
 
+const { wallet, signer } = storeToRefs(store)
+const { signMessageMemo, disconnect } = store
+const bookStoreApiStore = useBookStoreApiStore()
+const { authenticate } = bookStoreApiStore
+const { token, wallet: sessionWallet } = storeToRefs(bookStoreApiStore)
+
 const route = useRoute()
 const router = useRouter()
+const toast = useToast()
 
 onMounted(async () => {
   const { method, code } = route.query
@@ -23,8 +32,39 @@ onMounted(async () => {
       method: method as string,
       params: { code }
     })
+
+    if (signer.value) {
+      const signature = await signMessageMemo(
+        'authorize',
+        ['read:nftbook', 'write:nftbook', 'read:nftcollection', 'write:nftcollection']
+      )
+      if (!signature) {
+        disconnect()
+        router.replace('/')
+        return
+      }
+      await authenticate(wallet.value, signature)
+      try {
+        window.localStorage.setItem('likecoin_nft_book_press_token', JSON.stringify({ wallet: sessionWallet.value, token: token.value }))
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error(err)
+      }
+    } else {
+      toast.add({
+        icon: 'i-heroicons-exclamation-circle',
+        title: 'Failed to authenticate: no signer',
+        timeout: 0,
+        color: 'red',
+        ui: {
+          title: 'text-red-400 dark:text-red-400'
+        }
+      })
+    }
+
     const postAuthRoute = '/'
     router.replace(postAuthRoute)
   }
 })
+
 </script>
