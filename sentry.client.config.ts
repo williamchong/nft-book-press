b5eaa1cde4648e236c22ec9a8a28c20e3fa68ef3
William Chong <me@williamchong.cloud>
2024-09-12 02:06:33 +0800

    💩 Disable sentry in dev

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,9 +1,11 @@
 import * as Sentry from '@sentry/nuxt'
 
-Sentry.init({
-  dsn: 'https://1d871749c61de13371f3acd1681073b4@o149940.ingest.us.sentry.io/4507887589523456',
+if (process.env.NODE_ENV !== 'development') {
+  Sentry.init({
+    dsn: 'https://1d871749c61de13371f3acd1681073b4@o149940.ingest.us.sentry.io/4507887589523456',
 
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0
-})
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0
+  })
+}
