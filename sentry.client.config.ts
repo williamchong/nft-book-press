671d01ac1c9f422ddf679dc6ee2ab1e551f9d40a
William Chong <me@williamchong.cloud>
2024-09-13 01:22:25 +0800

    📈 Add sentry replay

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,8 +4,14 @@ if (process.env.NODE_ENV !== 'development') {
   Sentry.init({
     dsn: 'https://1d871749c61de13371f3acd1681073b4@o149940.ingest.us.sentry.io/4507887589523456',
 
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    tracesSampleRate: 1.0
+    tracesSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+    integrations: [
+      Sentry.replayIntegration({
+        maskAllText: true,
+        blockAllMedia: true
+      })
+    ]
   })
 }
