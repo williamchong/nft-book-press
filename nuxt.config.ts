8561497e4eac294e50df44fa8390bfe46bf0f23b
William Chong <me@williamchong.cloud>
2024-10-15 17:31:05 +0800

    🍱 Update favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -76,7 +76,12 @@ export default defineNuxtConfig({
   },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
-    layoutTransition: { name: 'page', mode: 'out-in' }
+    layoutTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+      ]
+    }
   },
   colorMode: {
     preference: 'light'
