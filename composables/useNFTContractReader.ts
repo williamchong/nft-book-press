58cb84478f05d39b9fd23a3d5a92b8b6ae2a527c
William Chong <me@williamchong.cloud>
2025-02-19 04:35:15 +0800

    🚧 Implement manual deliver via transferWithMemo

--- composables/useNFTContractReader.ts
+++ composables/useNFTContractReader.ts
@@ -32,8 +32,41 @@ export const useNFTContractReader = () => {
     })
   }
 
+  const getNFTMetadata = async (classId: string, tokenId: number) => {
+    const dataString = await readContract(config, {
+      abi: LIKE_NFT_CLASS_ABI,
+      address: classId as any,
+      functionName: 'tokenURI',
+      args: [tokenId]
+    })
+
+    if (!(dataString as string)?.startsWith('data:application/json')) {
+      return null
+    }
+
+    try {
+      const data = JSON.parse(
+        (dataString as string).replace('data:application/json;utf8,', '')
+      )
+      return data
+    } catch (e) {
+      return null
+    }
+  }
+
+  const getNFTOwner = async (classId: string, tokenId: number) => {
+    return await readContract(config, {
+      abi: LIKE_NFT_CLASS_ABI,
+      address: classId as any,
+      functionName: 'ownerOf',
+      args: [tokenId]
+    })
+  }
+
   return {
     getClassMetadata,
-    getClassOwner
+    getClassOwner,
+    getNFTMetadata,
+    getNFTOwner
   }
 }
