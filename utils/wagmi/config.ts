19b780ff642e9448ad83c7c80b998167dcaff821
William Chong <me@williamchong.cloud>
2025-04-03 12:30:29 +0800

    ✨ Add magic wagmi support

--- utils/wagmi/config.ts
+++ utils/wagmi/config.ts
@@ -1,10 +1,17 @@
 import { http, createStorage, cookieStorage, createConfig } from '@wagmi/vue'
 import { optimism, optimismSepolia } from '@wagmi/vue/chains'
 import { injected } from '@wagmi/connectors'
+import { dedicatedWalletConnector } from '@magiclabs/wagmi-connector'
 
 export const config = createConfig({
   chains: [optimism, optimismSepolia],
   connectors: [
+    dedicatedWalletConnector({
+      chains: [optimism, optimismSepolia],
+      options: {
+        apiKey: 'pk_live_5E14E3184484268D' // Liker Land Sepolia
+      }
+    }),
     injected()
   ],
   ssr: true,
