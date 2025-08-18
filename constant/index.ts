5eb9c2ea13c34f25233d941f6f5dbab5cecbe9f6
William Chong <me@williamchong.cloud>
2025-08-18 14:22:41 +0800

    👔 Lower max price tier

--- constant/index.ts
+++ constant/index.ts
@@ -2,7 +2,7 @@ export const IPFS_VIEW_GATEWAY_URL = 'https://ipfs.io/ipfs'
 
 export const MINIMAL_PRICE = 0.99
 export const DEFAULT_PRICE = 4.99
-export const MAXIMUM_PRICE = 199.99
+export const MAXIMUM_PRICE = 99.99
 
 export const USD_PRICING_OPTIONS = [0, ...Array.from(
   { length: Math.round(MAXIMUM_PRICE - MINIMAL_PRICE) + 1 },
