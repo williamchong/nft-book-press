e26a71b84228a271059306b471ab606048b3189b
William Chong <me@williamchong.cloud>
2026-02-06 23:57:33 +0800

    ⚙️ Update bulk upload CSV defaults: edition_name to 電子版, edition_description optional

--- constant/index.ts
+++ constant/index.ts
@@ -25,8 +25,8 @@ export const LIKE_ADDRESS_REGEX = /^like1[ac-hj-np-z02-9]{38}$/
 export const CSV_DEFAULT_LANGUAGE = 'zh'
 export const CSV_DEFAULT_AUTO_DELIVER = true
 export const CSV_DEFAULT_ENABLE_DRM = false
-export const CSV_DEFAULT_EDITION_NAME = '標準版'
-export const CSV_DEFAULT_EDITION_DESCRIPTION = '標準數位版'
+export const CSV_DEFAULT_EDITION_NAME = '電子版'
+export const CSV_DEFAULT_EDITION_DESCRIPTION = ''
 
 export const AFFILIATION_CHANNEL_DEFAULT = 'liker_land'
 
